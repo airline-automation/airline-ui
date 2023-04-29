4b5ddae67bbdc7c20784c54804affc8406bcec25
blitzkrieg <oguzhankuslar@gmail.com>
2023-04-29 21:51:39 +0300

    init airline-ui

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <React.Fragment>
+        <h1>Hello World!</h1>
+    </React.Fragment>
+);
